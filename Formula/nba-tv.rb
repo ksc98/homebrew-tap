697e6aaa619f617a9b338ea3900269e25e42d7df
Kyle <kyle@samaya.ai>
2026-04-22 16:52:45 -0700

    Bump nba-tv to v0.1.2

--- Formula/nba-tv.rb
+++ Formula/nba-tv.rb
@@ -1,15 +1,33 @@
 class NbaTv < Formula
   desc "Watch NBA streams in mpv from your terminal"
-  homepage "https://github.com/ksc98/nba-tv"
-  version "0.1.1"
+  homepage "https://github.com/ksc98/homebrew-tap"
+  version "0.1.2"
   license "MIT"
 
-  url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v0.1.1/nba-tv-macos-arm64.tar.gz"
-  sha256 "3e1d5f5ea1796841270c5580d8c791d205f3453ee272d9b45017b581e5f07215"
+  on_macos do
+    on_arm do
+      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v0.1.2/nba-tv-macos-arm64.tar.gz"
+      sha256 "7aec2700bdaa956d28c0e15ffad30c57df790df7596cb120f75a0b7623a96738"
+    end
+    on_intel do
+      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v0.1.2/nba-tv-macos-x86_64.tar.gz"
+      sha256 "ddf359deb25a9acfebcc3581a1174b2d31b90cae9dcf1c32caf689112cf3aa9e"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v0.1.2/nba-tv-linux-arm64.tar.gz"
+      sha256 "c346a4cf66b74d8495a588c566bca3ad2450efe3326878ad0c3e96effcba06c6"
+    end
+    on_intel do
+      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v0.1.2/nba-tv-linux-x86_64.tar.gz"
+      sha256 "41c6a949703d73e982b37d5c58548cdbb467ab4f6ca7b12fbbe28e54f119e902"
+    end
+  end
 
   depends_on "mpv"
-  depends_on :macos
-  depends_on arch: :arm64
+  depends_on "whisper-cpp" => :recommended
 
   def install
     bin.install "nba-tv"
