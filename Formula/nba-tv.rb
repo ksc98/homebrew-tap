7b3002d5dc09d0897adc0e51fa58df15ce5215a1
ksc98 <kylechang96@gmail.com>
2026-04-25 16:39:15 -0700

    Bump nba-tv to v1.5.5

--- Formula/nba-tv.rb
+++ Formula/nba-tv.rb
@@ -1,13 +1,13 @@
 class NbaTv < Formula
   desc "Watch NBA streams in mpv from your terminal"
   homepage "https://github.com/ksc98/homebrew-tap"
-  version "1.5.4"
+  version "1.5.5"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v1.5.4/nba-tv-macos-arm64.tar.gz"
-      sha256 "2492c97a44a8009fef11159289ef8ca536d073ebcc5d70fa61dda863fafbcf01"
+      url "https://github.com/ksc98/homebrew-tap/releases/download/nba-tv-v1.5.5/nba-tv-macos-arm64.tar.gz"
+      sha256 "910f3da566dfd7ea230917c2c9f98a51dcfdfa6ca887185f76e781898a805db3"
     end
   end
 
